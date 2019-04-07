93394c31fab67dd36f14ba8ac2fcbeca4701a87e
Minh Tuan <minhtuancse91@gmail.com>
2019-04-08 04:30:49 +0700

    - Init project - Install pod

---
+++ GroupExpense/GroupExpense/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GroupExpense
+//
+//  Created by Minh Tuan on 4/8/19.
+//  Copyright © 2019 Minh Tuan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ GroupExpense/Pods/Target Support Files/Pods-GroupExpenseTests/Pods-GroupExpenseTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GroupExpenseTests : NSObject
+@end
+@implementation PodsDummy_Pods_GroupExpenseTests
+@end
